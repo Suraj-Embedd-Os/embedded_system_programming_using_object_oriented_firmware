035dcd788900971c4c916942d6ef57b96248185c
EmbeddOs <suraj.dev120@gmail.com>
2021-02-01 20:45:55 +0530

    part-1 tested finished

--- led-programming-using-object-oriented/led.h
+++ led-programming-using-object-oriented/led.h
@@ -5,6 +5,21 @@
 #define __LED_H
 
 #include<stdint.h>
+#include "stm32f4xx.h"
+
+#define  LED_PORT				 GPIOA
+#define  LED1_PIN				(1<<7U)
+#define  LED2_PIN				(1<<8U)
+#define  LED3_PIN				(1<<4U)
+#define  LED4_PIN				(1<<5U)
+
+#define LED1_MODE_BIT		(1<<7*2)
+#define LED2_MODE_BIT		(1<<8*2)
+#define LED3_MODE_BIT		(1<<4*2)
+#define LED4_MODE_BIT		(1<<5*2)
+
+#define LED_CLOCK				(1<<0U)
+ 
 typedef enum{
 	
 	RED	=0,
